db5b3edf618336404dff8825eb5b22a7786f9bac
claudiaps <clp.sampedro@gmail.com>
2022-07-11 21:08:57 -0300

    criando novo arquivo e refatorando arquivo de soma

--- primeiroArquivo.js
+++ primeiroArquivo.js
@@ -1,6 +1,6 @@
 const primeiroNumero = 1;
 const segundoNumero = 4;
-var numeroTres = -9;
-const soma = primeiroNumero + segundoNumero + numeroTres;
-const subtracao = primeiroNumero - segundoNumero - numeroTres;
-console.log('Soma: ', soma, 'Subtracao: ', subtracao)
+const terceiroNumero = -9;
+const soma = primeiroNumero + segundoNumero + terceiroNumero;
+const subtracao = primeiroNumero - segundoNumero - terceiroNumero;
+console.log('Soma: ', soma, 'Subtracao: ', subtracao);
